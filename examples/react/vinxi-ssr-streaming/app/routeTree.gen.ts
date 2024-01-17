4ae786fca21ceea5348482d38337250b7c114bea
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-01-17 15:59:29 -0500

    add err route

--- examples/react/vinxi-ssr-streaming/app/routeTree.gen.ts
+++ examples/react/vinxi-ssr-streaming/app/routeTree.gen.ts
@@ -1,9 +1,15 @@
 import { Route as rootRoute } from './routes/__root'
 import { Route as HelloImport } from './routes/hello'
+import { Route as ErrorImport } from './routes/error'
 import { Route as IndexImport } from './routes/index'
 
 const HelloRoute = HelloImport.update({
   path: '/hello',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const ErrorRoute = ErrorImport.update({
+  path: '/error',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -17,10 +23,18 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/error': {
+      preLoaderRoute: typeof ErrorImport
+      parentRoute: typeof rootRoute
+    }
     '/hello': {
       preLoaderRoute: typeof HelloImport
       parentRoute: typeof rootRoute
     }
   }
 }
-export const routeTree = rootRoute.addChildren([IndexRoute, HelloRoute])
+export const routeTree = rootRoute.addChildren([
+  IndexRoute,
+  ErrorRoute,
+  HelloRoute,
+])
